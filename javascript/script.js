b01ee785d668fbf1d2c1edfae4459d03b615576b
zuzanatrembulakova <zuzanatrembulakova@users.noreply.github.com>
2020-02-19 15:56:33 +0100

    added javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,3 @@
-/*const link = "https://spreadsheets.google.com/feeds/list/1DVAg6s7LifjTSAXIaS7VHV0RBByKoWiT9NW8ay4at9Y/od6/public/values?alt=json"; window.addEventListener("DOMContentLoaded", getData);
-
-function getData(){
-    fetch(link)
-    .then(res => res.json())
-    .then(handleData)
-}
-
-function handleData(data){
-    const myData = data.feed.entry;
-    console.log(myData);
-    myData.forEach(showData);
-}
-
-const divmain = document.querySelector("main");
-
-function showData(item){
-
-    const template = document.querySelector("template").content;
-
-    var aCopy = template.cloneNode(true);
-    aCopy.querySelector("h2").textContent = item.gsx$id.$t;
-
-    divmain.appendChild(aCopy);
-}*/
 
 window.onscroll = function () {
     stickybar()
---
+++ javascript/spring.js
@@ -0,0 +1,29 @@
+const link = "https://spreadsheets.google.com/feeds/list/1DVAg6s7LifjTSAXIaS7VHV0RBByKoWiT9NW8ay4at9Y/od6/public/values?alt=json";
+window.addEventListener("DOMContentLoaded", getData);
+
+function getData() {
+    fetch(link)
+        .then(res => res.json())
+        .then(handleData)
+}
+
+function handleData(data) {
+    const myData = data.feed.entry;
+    console.log(myData);
+    myData.forEach(showData);
+}
+
+const divmain = document.querySelector("main");
+
+function showData(item) {
+
+    if (item.gsx$season.$t == "spring") {
+        const template = document.querySelector("template").content;
+
+        var aCopy = template.cloneNode(true);
+
+        aCopy.querySelector("h2").textContent = item.gsx$season.$t;
+
+        divmain.appendChild(aCopy);
+    }
+}
