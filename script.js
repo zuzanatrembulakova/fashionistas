811531789b788db8604d96feed4df8ddffe0923f
Zamira Grigiene <Zamira@Zamiras-MacBook-Pro.local>
2020-02-26 20:47:46 +0100

    added a modal

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ if (document.getElementById("script_args") != null) {
     var type = document.getElementById("script_args").getAttribute("arg1")
 };
 
+const modal = document.querySelector(".modal-background");
+modal.addEventListener("click", () => {
+    modal.classList.add("hide");
+});
+
 const link = "https://spreadsheets.google.com/feeds/list/1DVAg6s7LifjTSAXIaS7VHV0RBByKoWiT9NW8ay4at9Y/od6/public/values?alt=json";
 window.addEventListener("DOMContentLoaded", getData);
 
@@ -83,43 +88,56 @@ function showData(item) {
         } else {
             aCopy.querySelector(".discount").style.display = "none";
         }
-        document.querySelector(`#${item.gsx$category.$t}`).appendChild(aCopy);
-    }
 
-}
+        aCopy.querySelector(".read_more_btn").addEventListener("click", () => {
+                fetch(`https://spreadsheets.google.com/feeds/list/1DVAg6s7LifjTSAXIaS7VHV0RBByKoWiT9NW8ay4at9Y/od6/public/values?alt=json`)
+                    .then(res => res.json())
+                    .then(showDetails);
+        });
+                document.querySelector(`#${item.gsx$category.$t}`).appendChild(aCopy);
+            }
 
-function addToBasket(id) {
-    var buydata = localStorage.getItem("fashion_basket");
-    var buyarr = [];
-    if (buydata != null) {
-        buyarr = JSON.parse(buydata);
-    }
+        }
 
-    buyarr.push(id);
+        function addToBasket(id) {
+            var buydata = localStorage.getItem("fashion_basket");
+            var buyarr = [];
+            if (buydata != null) {
+                buyarr = JSON.parse(buydata);
+            }
 
-    localStorage.setItem("fashion_basket", JSON.stringify(buyarr));
+            buyarr.push(id);
 
-}
+            localStorage.setItem("fashion_basket", JSON.stringify(buyarr));
 
-
-window.onscroll = function () {
-    scrollFunction()
-};
+        }
 
 
-/*---GO TO TOP BTN-------------------------------------*/
+        window.onscroll = function () {
+            scrollFunction()
+        };
 
-function scrollFunction() {
-    var topBtn = document.getElementById("topBtn");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        topBtn.style.display = "block";
-    } else {
-        topBtn.style.display = "none";
-    }
-}
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() { // eslint-disable-line no-unused-vars
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+        /*---GO TO TOP BTN-------------------------------------*/
+
+        function scrollFunction() {
+            var topBtn = document.getElementById("topBtn");
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                topBtn.style.display = "block";
+            } else {
+                topBtn.style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() { // eslint-disable-line no-unused-vars
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+
+        function showDetails(data) {
+            modal.querySelector(".modal-name").textContent = data.name;
+            modal.querySelector(".modal-description").textContent = data.longdescription;
+            //...
+            modal.classList.remove("hide");
+        }
